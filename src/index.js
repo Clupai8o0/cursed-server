f4a4b860068185df0b0db18ad25b5eb0de8bd0e9
Clupai8o0 <clupaio4@gmail.com>
2021-08-18 09:10:26 +0300

    Cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const idGenerator = require("./algorithm");
+
+app.use(cors());
 
 //* database
 const mongodb = require("mongodb");
