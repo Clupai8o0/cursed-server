7b1fc4614bec96f03d0c76cb7ddd5a03d7adbafb
Clupai 8o0 <clupaio4@gmail.com>
2021-09-02 19:43:46 +0300

    db link

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_LOCALURL, {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   userCreateIndex: true, // indexes are created to allow quick access to data
 }); // ? to connect to database
